7ff4b1075ca92941470070452253934129f97ddb
Angielsor <banofinfinity@gmail.com>
2021-11-06 00:35:51 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int Counter = 0;
+            do
+            {
+                Counter++;
+                if (Counter == 100)
+                {
+                    MessageBox.Show("You caught me =)");
+                    this.Close();
+                }
+                int TP = random.Next(1, 5);
+                if (TP == 1)
+                {
+                    if (button1.Location.X + button1.Width < this.Width - 100)
+                    {
+                        button1.Location = new Point(button1.Location.X + button1.Width, button1.Location.Y);
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (TP == 2)
+                {
+                    if (button1.Location.X > 0)
+                    {
+                        button1.Location = new Point(button1.Location.X - button1.Width, button1.Location.Y);
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (TP == 3)
+                {
+                    if (button1.Location.Y + button1.Height < this.Height - 100)
+                    {
+                        button1.Location = new Point(button1.Location.X, button1.Location.Y + button1.Height);
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (TP == 4)
+                {
+                    if (button1.Location.Y > 0)
+                    {
+                        button1.Location = new Point(button1.Location.X, button1.Location.Y - button1.Height);
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            } while (true);
+        }
+    }
+}
